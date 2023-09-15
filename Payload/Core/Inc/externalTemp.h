944a97dbff9f0f8e9587cb80fbaf0a429b62684d
wb9coy <gene@wb9coy.com>
2023-09-14 21:11:52 -0700

    Delete Payload/Core directory

--- Payload/Core/Inc/externalTemp.h
+++
@@ -1,7 +0,0 @@
-#ifndef INC_EXTERNALTEMP_H_
-#define INC_EXTERNALTEMP_H_
-#include "ecc.h"
-
-int processExternalTemp(struct rscode_driver *rsDriver,float frequency);
-
-#endif /* INC_EXTERNALTEMP_H_ */
